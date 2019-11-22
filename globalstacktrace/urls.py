bdd1a20da4761bbfd38eea2caed404db3457e755
Hyunsik AEOM <t_aeom@naver.com>
2019-11-22 16:30:16 +0900

    sample web page, secret key 분리

--- globalstacktrace/urls.py
+++ globalstacktrace/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    # TODO web 애플리케이션 추가하기
     path('admin/', admin.site.urls),
+    path('', include('web.urls')),
 ]
