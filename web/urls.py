4c50056058e898f8a39c07e94d7f8a0d5b771498
Hyunsik AEOM <t_aeom@naver.com>
2019-11-29 18:10:40 +0900

    import materialize

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.signin, name='signin'),
+    # 로그인
+    path('signin/', views.signin, name='signin'),
+    # 회원가입
+    path('signup/', views.signup, name='signup'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     return render(request, 'web/index.html')
 
+# TODO 로그인 페이지 구현
 def signin(request):
-    
     return render(request, 'web/signin.html')
+
+# TODO 회원가입 페이지 구현
+def signup(request):
+    return render(request, 'web/signup.html')
